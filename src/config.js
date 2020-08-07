ef4f1015e553f578543a2c83253168f76fe524db
David <rembao1@gmail.com>
2020-08-06 20:28:45 -0700

    New values for config file

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 export default {
     s3: {
       REGION: "us-east-1",
-      BUCKET: "notes-app-uploads-rbd"
+      BUCKET: "notes-app-2-api-drembao-attachmentsbucket-13hanb5g4k0k9"
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: "https://zc5b523000.execute-api.us-east-1.amazonaws.com/prod"
+      URL: "https://xvcdbnzv16.execute-api.us-east-1.amazonaws.com/drembao"
     },
     cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_RVDG9mS9i",
-      APP_CLIENT_ID: "1safntcqjnv5nahj2un11kkg96",
-      IDENTITY_POOL_ID: "us-east-1:5c0c1e40-2fcb-4585-a79b-b8cf2477acab"
+      USER_POOL_ID: "us-east-1_SypKyHSM6",
+      APP_CLIENT_ID: "4iqhoskl8669402dft9abda00l",
+      IDENTITY_POOL_ID: "us-east-1:74a72dd8-7387-4e64-a3dc-7e2360d0bb21"
     },
     MAX_ATTACHMENT_SIZE: 5000000,
     STRIPE_KEY: "pk_test_51HCsFZLesJgBqV0P5VEeWipuzcnqs0v7NdQ5KSCnQnJEAewRLExNMM38KSEJigHi29WQqjCsUZAJVhiMJypw2hJM00TPMbFjDy"
